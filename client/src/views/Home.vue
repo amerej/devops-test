fe20415f2499ff278442f9bc232d0d40ed8c134d
Audran Ditsch <audran.ditsch@outlook.fr>
2018-09-13 09:15:44 +0200

    Add k8s

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App v2"/>
   </div>
 </template>
 
