6b173f1d78aa598c5a403624e31ab63e6e560723
Audran Ditsch <audran.ditsch@outlook.fr>
2018-09-13 13:01:20 +0200

    Add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker build -t amerej/devops-test-client:latest -t amerej/devops-test-client:$SHA -f ./client/Dockerfile ./client
+docker build -t amerej/devops-test-server:latest -t amerej/devops-test-server:$SHA -f ./server/Dockerfile ./server
+
+docker push amerej/devops-test-client:latest
+docker push amerej/devops-test-server:latest
+
+docker push amerej/devops-test-client:$SHA
+docker push amerej/devops-test-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=amerej/devops-test-server:$SHA
+kubectl set image deployments/client-deployment server=amerej/devops-test-client:$SHA
